ee15a65accef3e3df56fc0e066a9dbedb42125e7
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-02-10 21:27:53 -0600

    add output tag

--- train_student.sh
+++ train_student.sh
@@ -1,7 +1,12 @@
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./train_student.sh <tag>"
+    exit
+fi
+tag=$1
 python train_student_encoder.py \
 teacher_model_file=/home/cc/code/catalog/dpr/downloads/checkpoint/retriever/single-adv-hn/nq/bert-base-encoder.cp \
 train=biencoder_nq \
 train_datasets=[nq_train_hn1] \
 dev_datasets=[nq_dev] \
-output_dir=student_encoder \
+output_dir=student_encoder_${tag} \
 
