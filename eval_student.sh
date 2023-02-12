f17b36bacc2a61d5d3041367d6562a3a8c13301c
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-02-11 23:08:38 -0600

    update model file

--- eval_student.sh
+++ eval_student.sh
@@ -1,7 +1,7 @@
 python train_student_encoder.py \
 train=biencoder_nq \
+model_file=/home/cc/code/catalog/dpr/trained_models/student_8/dpr_student_biencoder.18 \
 ta_layers=1 \
-model_file=/home/cc/code/catalog/dpr/trained_models/student_8/dpr_student_biencoder.12 \
 dev_datasets=[nq_dev] \
 output_dir=student_eval \
 ignore_checkpoint_optimizer=True \
