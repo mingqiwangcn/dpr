269e5607576f97dd4ee09e2e97ad69756fc10a78
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-02-12 16:44:24 -0600

    update model file

--- train_student.sh
+++ train_student.sh
@@ -5,8 +5,8 @@ fi
 tag=$1
 python train_student_encoder.py \
 teacher_is_ta=true \
-ta_layers=6 \
-teacher_model_file=/home/cc/code/catalog/dpr/outputs/2023-02-10/21-14-18/student_encoder/dpr_student_biencoder.4 \
+ta_layers=3 \
+teacher_model_file=/home/cc/code/catalog/dpr/outputs/2023-02-10/22-48-10/student_encoder_keep_0_6_11/dpr_student_biencoder.18 \
 train=biencoder_nq \
 train_datasets=[nq_train_hn1] \
 dev_datasets=[nq_dev] \
