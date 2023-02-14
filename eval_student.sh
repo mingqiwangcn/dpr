92451c3f7fe07dfa5ea2ba68cd9d56b5ce23d0ab
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-02-14 13:12:46 -0600

    update new model file

--- eval_student.sh
+++ eval_student.sh
@@ -1,8 +1,8 @@
 python train_student_encoder.py \
 train=biencoder_nq \
 eval_type=rank \
-model_file=/home/cc/code/catalog/dpr/trained_models/student_8/dpr_student_biencoder.27 \
-ta_layers=1 \
+model_file=/home/cc/code/catalog/dpr/outputs/2023-02-13/23-46-25/student_encoder_test/dpr_student_biencoder.36 \
+ta_layers=-1 \
 dev_datasets=[nq_dev] \
 output_dir=student_eval \
 ignore_checkpoint_optimizer=True \
--- train_student.sh
+++ train_student.sh
@@ -6,7 +6,7 @@ tag=$1
 python train_student_encoder.py \
 teacher_is_ta=true \
 ta_layers=3 \
-teacher_model_file=/home/cc/code/catalog/dpr/outputs/2023-02-10/22-48-10/student_encoder_keep_0_6_11/dpr_student_biencoder.18 \
+teacher_model_file=/home/cc/code/catalog/dpr/trained_models/ta_student_4/dpr_student_biencoder.18 \
 train=biencoder_nq \
 train_datasets=[nq_train_hn1] \
 dev_datasets=[nq_dev] \
