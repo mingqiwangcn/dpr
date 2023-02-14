02110cdf7a6b5a8921900e948cb478741ca2a9e3
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-02-14 16:48:10 -0600

    update args

--- train_student.sh
+++ train_student.sh
@@ -1,12 +1,14 @@
-if [ "$#" -ne 1 ]; then
-    echo "Usage: ./train_student.sh <tag>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: ./train_student.sh <tag> <teacher_model_file> <student layers>"
     exit
 fi
 tag=$1
+teacher_model=$2
+layers=$3
 python train_student_encoder.py \
 teacher_is_ta=false \
-teacher_model_file=/home/cc/code/catalog/dpr/downloads/checkpoint/retriever/single-adv-hn/nq/bert-base-encoder.cp \
-student_layers=[1,2,3,4,5,6,7,8,9,10,11] \
+teacher_model_file=${teacher_model} \
+student_layers=${layers} \
 train=biencoder_nq \
 train_datasets=[nq_train_hn1] \
 dev_datasets=[nq_dev] \
