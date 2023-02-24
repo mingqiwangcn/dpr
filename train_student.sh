e2355a0f4e322b14edaa8c6e2b7970c503942345
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-02-24 00:15:12 -0600

    teacher name

--- train_student.sh
+++ train_student.sh
@@ -1,17 +1,21 @@
-if [ "$#" -ne 4 ]; then
-    echo "Usage: ./train_student.sh <tag> <teacher_is_ta> <teacher_model_file> <student layers>"
+if [ "$#" -ne 5 ]; then
+    echo "Usage: ./train_student.sh <teacher_name> <teacher_is_ta> <teacher_model_file> <student_name> <student layers>"
     exit
 fi
-tag=$1
+
+teacher_name=$1
 is_ta=$2
 teacher_model=$3
-layers=$4
+student_name=$4
+layers=$5
+
 python train_student_encoder.py \
 teacher_is_ta=${is_ta} \
+teacher_name=${teacher_name} \
 teacher_model_file=${teacher_model} \
 student_layers=${layers} \
 train=biencoder_nq \
 train_datasets=[nq_train_hn1] \
 dev_datasets=[nq_dev] \
-output_dir=student_encoder_${tag} \
+output_dir=encoder_${teacher_name}_${student_name} \
 score_temperature=6 \
