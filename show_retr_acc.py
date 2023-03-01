e362a1a488a9b66bf3d9fc5303a93920a31c2418
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-02-28 20:28:22 -0600

    compare teacher and student

--- show_retr_acc.py
+++ show_retr_acc.py
@@ -1,5 +1,6 @@
 import json
 import numpy as np
+from tqdm import tqdm
 
 def show_acc(tag, rank_file, top_k_lst):
     res_dict = {}
@@ -18,16 +19,65 @@ def show_acc(tag, rank_file, top_k_lst):
         report += '  R@%d = %.2f' % (top_k, top_k_acc)
     print(report)  
 
+
+def read_nll_score(data_file):
+    num_correct = 0
+    num_total = 0
+    correct_set = set()
+    with open(data_file) as f:
+        for line in tqdm(f):
+            item = json.loads(line)
+            ctxs = item['ctxs']
+            query_lst = item['queries']
+            for query in query_lst:
+                num_total += 1
+                pos_index = query['pos_ctx_index']
+                scores = query['scores']
+                best_idx = np.argmax(scores)
+                if best_idx == pos_index:
+                    num_correct += 1
+                    correct_set.add(query['index'])
+
+    print('correct ratio = %d/%d = %f' % (num_correct, num_total, num_correct / num_total))
+    return correct_set
+
+
+def cmp_teacher_student():
+    correct_teacher_set = read_nll_score('./outputs/2023-02-28/18-42-29/scores.jsonl') 
+    correct_student_set = read_nll_score('./outputs/2023-02-28/18-11-59/scores.jsonl') 
+    
+    teacher_1_student_0 = correct_teacher_set - correct_student_set  
+    teacher_0_student_1 = correct_student_set - correct_teacher_set
+    
+    print('teacher_1_student_0 = %d' % len(teacher_1_student_0))
+    print('teacher_0_student_1 = %d' % len(teacher_0_student_1))
+
 def main():
+    cmp_teacher_student()    
+       
+
+def show_rank_accuracy():
     top_k_lst = [20, 50, 100, 150, 200]
    
-    teacher_rank_file = './outputs/2023-02-11/23-09-57/rank.jsonl'
-    show_acc('Teacher', teacher_rank_file, top_k_lst)
-     
-    ta_rank_file = './outputs/2023-02-11/23-10-59/rank.jsonl'
-    show_acc('TA', ta_rank_file, top_k_lst)
-     
-    student_rank_file = './outputs/2023-02-14/12-44-31/rank.jsonl'
+    student_rank_file = './outputs/2023-02-26/13-37-08/rank.jsonl'
+    show_acc('Student', student_rank_file, top_k_lst)
+    
+    student_rank_file = './outputs/2023-02-26/17-16-33/rank.jsonl'
+    show_acc('Student', student_rank_file, top_k_lst)
+
+    student_rank_file = './outputs/2023-02-27/00-45-45/rank.jsonl'
+    show_acc('Student', student_rank_file, top_k_lst)
+    
+    student_rank_file = './outputs/2023-02-27/12-01-55/rank.jsonl'
+    show_acc('Student', student_rank_file, top_k_lst)
+    
+    student_rank_file = './outputs/2023-02-27/17-00-36/rank.jsonl'
+    show_acc('Student', student_rank_file, top_k_lst)
+    
+    student_rank_file = './outputs/2023-02-28/00-00-47/rank.jsonl'
+    show_acc('Student', student_rank_file, top_k_lst)
+    
+    student_rank_file = './outputs/2023-02-28/10-01-20/rank.jsonl'
     show_acc('Student', student_rank_file, top_k_lst)
 
 if __name__ == '__main__':
