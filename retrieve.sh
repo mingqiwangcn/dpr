8980be06e72ef1165ba20d150cf6f4f910871502
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-03-04 23:28:38 -0600

    update model and emb file

--- retrieve.sh
+++ retrieve.sh
@@ -7,7 +7,7 @@ indexer=$1
 fac_str=$2
 tag=$3
 is_teacher=false
-model=/home/cc/code/catalog/dpr/train_models/dpr_student_precom_16_biencoder.71
+model=/home/cc/code/catalog/dpr/trained_models/dpr_student_precom_16_biencoder.71
 python dense_retriever.py \
 is_teacher=${is_teacher} \
 model_file=${model} \
@@ -15,6 +15,6 @@ indexer=${indexer} \
 factory_string=\"${fac_str}\" \
 qa_dataset=nq_test \
 ctx_datatsets=[dpr_wiki] \
-encoded_ctx_files=[\"/home/cc/code/dpr/passage_embs/student_1_layer_16_neg_embs/wiki_emb_0_part_*\"] \
+encoded_ctx_files=[\"/home/cc/code/catalog/dpr/passage_embs/student_1_layer_16_neg_embs/wiki_emb_0_part_*\"] \
 out_file=retrieval_${tag}.json \
 
